b3facdb97f749a0dd210a496882d885c7af35e61
Marcos Beltrão <marcos.beltrao@outlook.pt>
2025-11-12 16:29:19 -0300

    feat: reorganize exports in index.ts for improved structure and clarity

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,20 @@
+// Generators
 export * from "./generators/initProject";
 export * from "./generators/addFeature";
 export * from "./generators/addEntity";
+export * from "./generators/segments";
+
+// Types
+export * from "./types";
+
+// Utils
+export * from "./utils/logger";
+export * from "./utils/errors";
+export * from "./utils/fs";
+
+// Config management
+export * from "./config";
+
+// Validators
+export * from "./validators/nameValidator";
+export * from "./validators/segmentValidator";
